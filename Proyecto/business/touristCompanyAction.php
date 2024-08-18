4d98a592b501b536051cc06150601802acc652f0
Glend <glendrojas1040@gmail.com>
2024-08-17 21:51:36 -0600

    Creada el Action y el business
    a

--- Proyecto/business/touristCompanyAction.php
+++ Proyecto/business/touristCompanyAction.php
@@ -1,40 +1,68 @@
 <?php
-require_once 'touristCompanyBusiness.php';
-include './OwnerBusiness.php';
 
-if (isset($_POST['action'])) {
-    $touristCompanyBusiness = new TouristCompanyBusiness();
+include_once '../data/touristCompanyBusiness.php';
+include_once '../domain/TouristCompany.php';
 
-    switch ($_POST['action']) {
-        case 'create':
-            $touristCompany = new TouristCompany(
-                $_POST['id'],
-                $_POST['legalName'],
-                $_POST['magicName'],
-                (new OwnerBusiness())->getTBOwner($_POST['ownerId']),
-                (new CompanyTypeBusiness())->getCompanyTypeById($_POST['companyTypeId'])
-            );
-            $touristCompanyBusiness->createTouristCompany($touristCompany);
-            break;
+if(isset($_POST['create'])){
+    if(isset($_POST['legalName'])){
+        if(isset($_POST['magicName'])){
+            if(isset($_POST['owner'])){
+                if(isset($_POST['companyType'])){
+                    if(isset($_POST['status'])){
+                        $legalName = $_POST['legalName'];
+                        $magicName = $_POST['magicName'];
+                        $owner = $_POST['owner'];
+                        $companyType = $_POST['companyType'];
+                        $status = $_POST['status'];
 
-        case 'update':
-            $touristCompany = new TouristCompany(
-                $_POST['id'],
-                $_POST['legalName'],
-                $_POST['magicName'],
-                (new OwnerBusiness())->getTBOwner($_POST['ownerId']),
-                (new CompanyTypeBusiness())->getCompanyTypeById($_POST['companyTypeId'])
-            );
-            $touristCompanyBusiness->updateTouristCompany($touristCompany);
-            break;
+                        if(strlen(trim($legalName)) > 0 && strlen(trim($magicName)) > 0 && strlen(trim($owner)) > 0 && $companyType instanceof CompanyType && strlen(trim($status)) > 0){
+                            if(!is_numeric($legalName) && !is_numeric($magicName) && !is_numeric($owner) && is_numeric($status)){
+                                $touristCompany = new TouristCompany(0, $legalName, $magicName, $owner, $companyType, $status);
 
-        case 'delete':
-            $touristCompanyBusiness->deleteTouristCompany($_POST['id']);
-            break;
+                                $touristCompanyBusiness = new TouristCompanyBusiness();
 
-        case 'getAll':
-            echo json_encode($touristCompanyBusiness->getTouristCompanies());
-            break;
+                                $result = $touristCompanyBusiness->insert($touristCompany);
+
+                                if($result == 1){
+                                    header("location: ../view/touristCompanyView.php?success=inserted");
+                                    exit();
+                                }else if($result == null){
+                                    header("location: ../view/touristCompanyView.php?error=alreadyexists");
+                                    exit();
+                                }else{
+                                    header("location: ../view/touristCompanyView.php?error=dbError");
+                                    exit();
+                                }
+                            }else{
+                                header("location: ../view/touristCompanyView.php?error=numberFormat");
+                                exit();
+                            }
+                        }else{
+                            header("location: ../view/touristCompanyView.php?error=emptyField");
+                            exit();
+                        }
+                    }else{
+                        header("location: ../view/touristCompanyView.php?error=error");
+                        exit();
+                    }
+                }else{
+                    header("location: ../view/touristCompanyView.php?error=errorcompanyType");
+                    exit();
+                }
+            }else{
+                header("location: ../view/touristCompanyView.php?error=errorInOwner");
+                exit();
+            }
+        }else{
+            header("location: ../view/touristCompanyView.php?error=errorInMagicName");
+            exit();
+        }
+    }else{
+        header("location: ../view/touristCompanyView.php?error=errorInLegalName");
+        exit();
     }
 }
+
+
+
 ?>
--- Proyecto/business/touristCompanyBusiness.php
+++ Proyecto/business/touristCompanyBusiness.php
@@ -1,24 +1,33 @@
 <?php
+
+include_once '../data/touristCompanyData.php';
+include_once '../domain/TouristCompany.php';
 class TouristCompanyBusiness {
+
     private $touristCompanyData;
 
     public function __construct() {
         $this->touristCompanyData = new TouristCompanyData();
     }
 
-    public function createTouristCompany($touristCompany) {
+    public function insert($touristCompany) {
         return $this->touristCompanyData->insertTouristCompany($touristCompany);
     }
 
-    public function getTouristCompanies() {
+    public function getAll() {
         return $this->touristCompanyData->getAllTouristCompanies();
     }
 
-    public function updateTouristCompany($touristCompany) {
+    public function delete($idTouristCompany) {
+        return $this->touristCompanyData->deleteTouristCompany($idTouristCompany);
+    }
+
+    public function update($touristCompany) {
         return $this->touristCompanyData->updateTouristCompany($touristCompany);
     }
 
-    public function deleteTouristCompany($id) {
-        return $this->touristCompanyData->deleteTouristCompany($id);
+    public function getById($idTouristCompany) {
+        return $this->touristCompanyData->getTouristById($idTouristCompany);
     }
+    
 }
