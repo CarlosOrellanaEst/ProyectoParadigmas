8e37e9c591d5dadaa3244ead966712369a71ef92
Glend <glendrojas1040@gmail.com>
2024-08-17 23:28:55 -0600

    Update dbecotouristiar.sql
    arreglo en sql

--- Proyecto/backup/dbecotouristiar.sql
+++ Proyecto/backup/dbecotouristiar.sql
@@ -36,21 +36,8 @@ CREATE TABLE `tbbankaccount` (
   `tbbankaccountisactive` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `tbbankaccount`
---
-
-INSERT INTO `tbbankaccount` (`tbbankaccountid`, `tbbankaccountownerid`, `tbbankaccountnumber`, `tbbankaccountbankname`, `tbbankaccountstatus`, `tbbankaccountisactive`) VALUES
-(1, 10, '1111111', 'bcr', 1, 0),
-(2, 2, '33', 'ddd', 0, 0),
-(3, 10, '5', 'sdf', 1, 0),
-(4, 10, '5645', 'dfgdfg', 0, 0),
-(5, 10, '22', 'ddd', 1, 0),
-(6, 10, '4300', 'na', 0, 0),
-(7, 2, '736464', 'jdd', 1, 0);
 
 -- --------------------------------------------------------
-
 --
 -- Estructura de tabla para la tabla `tbowner`
 --
@@ -65,7 +52,6 @@ CREATE TABLE `tbowner` (
   `tbownerdirection` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `tbownerstatus` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 -- --------------------------------------------------------
 
 --
@@ -78,15 +64,6 @@ CREATE TABLE `tbroll` (
   `tbrolldescription` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `tbrollstatus` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `tbroll`
---
-
-INSERT INTO `tbroll` (`tbrollid`, `tbrollname`, `tbrolldescription`, `tbrollstatus`) VALUES
-(1, 'Administrador', 'Acceso completo a todas las funciones y configuraciones del sistema.', 1),
-(2, 'Turista', 'Potencial cliente de los negocios registrados en el sistema', 1),
-(3, 'Propietario', '', 1);
 
 -- --------------------------------------------------------
 
@@ -103,6 +80,45 @@ CREATE TABLE `tbuser` (
   `tbuserstatus` tinyint(1) NOT NULL,
   `tbusertype` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- --------------------------------------------------------	
+-- Estructura de tabla para la tabla `tbtouristcompanytype`
+--
+
+CREATE TABLE `tbtouristcompanytype` (
+  `tbtouristcompanytypeid` int(11) NOT NULL,
+  `tbtouristcompanytypename` varchar(100) NOT NULL,
+  `tbtouristcompanytypedescription` varchar(250) NOT NULL,
+  `tbtouristcompanytypeisactive` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `tbroll`
+--
+
+-- --------------------------------------------------------
+--
+-- Volcado de datos para la tabla `tbbankaccount`
+--
+
+INSERT INTO `tbbankaccount` (`tbbankaccountid`, `tbbankaccountownerid`, `tbbankaccountnumber`, `tbbankaccountbankname`, `tbbankaccountstatus`, `tbbankaccountisactive`) VALUES
+(1, 10, '1111111', 'bcr', 1, 0),
+(2, 2, '33', 'ddd', 0, 0),
+(3, 10, '5', 'sdf', 1, 0),
+(4, 10, '5645', 'dfgdfg', 0, 0),
+(5, 10, '22', 'ddd', 1, 0),
+(6, 10, '4300', 'na', 0, 0),
+(7, 2, '736464', 'jdd', 1, 0);
+
+
+-- --------------------------------------------------------
+--
+-- Volcado de datos para la tabla `tbroll`
+--
+INSERT INTO `tbroll` (`tbrollid`, `tbrollname`, `tbrolldescription`, `tbrollstatus`) VALUES
+(1, 'Administrador', 'Acceso completo a todas las funciones y configuraciones del sistema.', 1),
+(2, 'Turista', 'Potencial cliente de los negocios registrados en el sistema', 1),
+(3, 'Propietario', '', 1);
+
 
 --
 -- Volcado de datos para la tabla `tbuser`
@@ -115,15 +131,6 @@ INSERT INTO `tbuser` (`tbuserid`, `tbusername`, `tbuserlastname`, `tbuserpasswor
 (4, 'alice', 'Wonderland', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', '7654321098', 1, 'Turista'),
 (5, 'bob', 'Builder', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '8765432109', 1, 'Propietario');
 
--- Estructura de tabla para la tabla `tbtouristcompanytype`
---
-
-CREATE TABLE `tbtouristcompanytype` (
-  `tbtouristcompanytypeid` int(11) NOT NULL,
-  `tbtouristcompanytypename` varchar(100) NOT NULL,
-  `tbtouristcompanytypedescription` varchar(250) NOT NULL,
-  `tbtouristcompanytypeisactive` tinyint(1) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `tbtouristcompanytype`
@@ -139,17 +146,6 @@ INSERT INTO `tbtouristcompanytype` (`tbtouristcompanytypeid`, `tbtouristcompanyt
 (5, 'Actividades Recreativas', 'Organización de actividades recreativas y eventos para turistas.', 1);
 COMMIT;
 
-CREATE TABLE `tbowner` (
-  `tbownerid` int(11) NOT NULL,
-  `tbownername` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tbownersurnames` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tbownerlegalidentification` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tbownerphone` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tbowneremail` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tbownerdirection` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tbownerstatus` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 --
 -- Volcado de datos para la tabla `tbowner`
 --
@@ -161,12 +157,7 @@ INSERT INTO `tbowner` (`tbownerid`, `tbownername`, `tbownersurnames`, `tbownerle
 (4, 'Marta', 'Cruz García', '44332211', '555-1122', 'marta.cruz@example.com', 'Plaza Mayor 5, Ciudad', 1),
 (5, 'José', 'Sánchez Romero', '55667788', '555-3344', 'jose.sanchez@example.com', 'Calle de los Naranjos 20, Ciudad', 1);
 COMMIT;
--- Índices para tablas volcadas
---
 
---
--- Indices de la tabla `tbowner`
---
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
