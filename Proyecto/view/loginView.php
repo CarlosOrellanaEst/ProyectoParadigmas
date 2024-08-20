bbab5233659ef83e845ac77b508f53a37722ac26
Glend <glendrojas1040@gmail.com>
2024-08-19 22:23:20 -0600

    Cambios en el index
    Cambios de ruta en login, empresaturistica y roll

--- Proyecto/view/loginView.php
+++ Proyecto/view/loginView.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <button><a href="index.html">Index</a></button>
+    <button><a href="../index.html">Index</a></button>
     <title>Iniciar Sesión</title>
 </head>
 <body>
